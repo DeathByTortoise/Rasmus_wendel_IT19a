7fd752ad28411c7e2c1aa8c564daf3bec7bb86a5
DeathByTortoise <rasmus.wendel.1@gmil.com>
2020-09-01 09:41:26 +0200

    wow

---
+++ tillämpad programering/html/script.js
@@ -0,0 +1,16 @@
+let first = "Sarah"
+let last = "C"
+let cereal = "0000001"
+let age = "N/A"
+let dob = "2020-09-01 08:30"
+let dod = "2020-09-01 09:45"
+
+
+
+
+console.log(`Hello my name is ${first} ${last} cereal number:${cereal} I am ${age} old and was born ${dob}`)
+if (cereal <= 0000001){
+    console.log("I'm the first of my kind in a line of adroids")
+} else {
+    console.log("I am not the first of my kind but I asure you i will be of use")
+}
